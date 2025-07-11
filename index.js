a0e19306199365829ee33315dfa3a3fd8d7161d0
Jorge Vasconcelos <vasconcelos.vsouza@gmail.com>
2025-07-11 19:24:31 -0300

    finished the basic of JWT authentication

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const jwt = require("jsonwebtoken");
+const { error } = require("console");
 const SECRET = "jorgeaereo";
 
 app.use(bodyParser.json());
@@ -10,7 +11,25 @@ app.use(bodyParser.json());
 app.get("/", (req, res) => {
   res.json({ message: "Tudo ok por aqui!" });
 });
-app.get("/clientes", (req, res) => {
+
+function verifyJWT(req, res, next) {
+  const token = req.headers["x-access-token"];
+  const index = blacklist.findIndex((item) => (item = token));
+  if (index !== -1) {
+    return res.status(401).end();
+  }
+
+  jwt.verify(token, SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).end();
+    }
+    req.userId = decoded.userId;
+    next();
+  });
+}
+
+app.get("/clientes", verifyJWT, (req, res) => {
+  console.log(req.userId + " fez esta chamada!!");
   res.json([{ id: 1, nome: "jorge" }]);
 });
 
@@ -23,7 +42,10 @@ app.post("/login", (req, res) => {
   res.status(401).end();
 });
 
+const blacklist = [];
+
 app.post("/logout", function (req, res) {
+  blacklist.push(req.headers["x-access-token"]);
   res.end();
 });
 
